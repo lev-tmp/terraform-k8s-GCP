637b2cf859a3a294dec9b3ebc7a313ca67ed8766
lev-tmp <33494698+lev-tmp@users.noreply.github.com>
2018-10-18 18:22:52 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@
 provider "google" {
   credentials = "${file("<you credential file>")}"
   project     = "<your project>"
-  region      = "<region where cluster will be deploy>"
+  region      = "us-central1"
 }
