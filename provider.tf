2e5f8acbf1176f22498a06b3db39d1dedb111f79
lev-tmp <33494698+lev-tmp@users.noreply.github.com>
2018-10-18 18:20:17 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 // Configure the Google Cloud provider
 provider "google" {
-  credentials = "${file("master-well-208020-1f0384288a4c.json")}"
-  project     = "master-well-208020"
+  credentials = "${file("<you credential file>")}"
+  project     = "<your project>"
   region      = "us-central1"
 }
